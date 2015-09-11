e063c36b14a9884adb47dcb911b78246f4ba07be
Matthias Dumke <matthias.dumke@gmx.net>
2015-09-11 22:23:31 +0200

    add category-model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150911194043) do
+ActiveRecord::Schema.define(version: 20150911194924) do
+
+  create_table "categories", force: true do |t|
+    t.string "name"
+  end
 
   create_table "comments", force: true do |t|
     t.text     "body"
