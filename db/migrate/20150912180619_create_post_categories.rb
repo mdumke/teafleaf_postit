8908c473e743e3feb32665311f62ae14335dfd59
Matthias Dumke <matthias.dumke@gmx.net>
2015-09-12 20:14:23 +0200

    add M:M association between posts and categories

---
+++ db/migrate/20150912180619_create_post_categories.rb
@@ -0,0 +1,10 @@
+class CreatePostCategories < ActiveRecord::Migration
+  def change
+    create_table :post_categories do |t|
+      t.integer :post_id, index: true
+      t.integer :category_id, index: true
+
+      t.timestamps null: false
+    end
+  end
+end
