c4192a8a85d5ca196e6eee1805ae5f3f35ae6d53
Matthias Dumke <matthias.dumke@gmx.net>
2015-09-13 17:46:32 +0200

    implement index and show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
+  # GET /posts
   def index
+    @posts = Post.all
+  end
 
+  # GET /posts/:id
+  def show
+    @post = Post.find(params[:id])
+    redirect_to 'index' unless @post
+
+    @comments = @post.comments
   end
 end
