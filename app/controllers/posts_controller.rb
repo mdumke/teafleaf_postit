be9e51ab722098a2c7057daebf0d705f92c245c4
Matthias Dumke <matthias.dumke@gmx.net>
2015-09-14 23:15:39 +0200

    update views to match lecture-code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,25 @@ class PostsController < ApplicationController
 
   # GET /posts/:id
   def show
-    @post = Post.find(params[:id])
+    @post = Post.find_by_id(params[:id])
     redirect_to 'index' unless @post
 
     @comments = @post.comments
+  end
+
+  # GET /posts/new
+  def new
+  end
+
+  # POST /posts
+  def create
+  end
+
+  # GET /posts/:id/edit
+  def edit
+  end
+
+  # PATCH /posts/:id
+  def update
   end
 end
