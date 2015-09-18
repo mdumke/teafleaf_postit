723eb0cb72f1e2668c1f5e3f3c90d1245b898877
Matthias Dumke <matthias.dumke@gmx.net>
2015-09-18 22:41:04 +0200

    implement basic actions categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,36 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash['notice'] = 'Category has been created'
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_category
+    @category = Category.find_by_id(params[:id])
+
+    unless @category
+      flash['error'] = 'Could not find this category'
+      redirect_to root_route
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
