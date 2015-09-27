ded2bea8087cdd10951b07b038c947f0446defd4
Matthias Dumke <matthias.dumke@gmx.net>
2015-09-27 14:12:25 +0200

    implement registering for users

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ PostitTemplate::Application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  get '/register', to: 'users#new'
 
   resources :posts, except: [:destroy] do
     resources :comments, only: [:create]
   end
 
   resources :categories, only: [:new, :create, :show]
+  resources :users, only: [:show, :create, :edit, :update]
 end
