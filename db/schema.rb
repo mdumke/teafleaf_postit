67c872c152e672fb12717512cdf7a6c9ddec301d
Matthias Dumke <matthias.dumke@gmx.net>
2015-10-05 13:27:54 +0200

    add slug-column to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150929090133) do
+ActiveRecord::Schema.define(version: 20151005112705) do
 
   create_table "categories", force: true do |t|
     t.string "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150929090133) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "slug"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
